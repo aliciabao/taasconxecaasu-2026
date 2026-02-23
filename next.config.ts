56362e78e022acf0037ed90e258665e8e030abc9
Alicia <aliciasbao@gmail.com>
2026-02-22 23:04:33 -0500

    deploy configs

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/taasconxecaasu-2026/",
+  assetPrefix: "/taasconxecaasu-2026/",
+
 };
 
 export default nextConfig;
