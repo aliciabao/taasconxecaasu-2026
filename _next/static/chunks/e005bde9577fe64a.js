8858afd424848dfab1d0e1624fe600997f83233d
Alicia <aliciasbao@gmail.com>
2026-02-24 03:02:06 -0500

    Updates

--- _next/static/chunks/e005bde9577fe64a.js
+++ _next/static/chunks/e005bde9577fe64a.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/11e36138c3747731.js",
   "static/chunks/ab2f30348212da7f.js",
   "static/chunks/db2bfe74d307fa57.js",
-  "static/chunks/03607a5b1450943a.css",
-  "static/chunks/turbopack-32b108f1d0dfe585.js"
+  "static/chunks/172b3f9850a7e7ef.css",
+  "static/chunks/turbopack-bc18f671516756f8.js"
 ])
